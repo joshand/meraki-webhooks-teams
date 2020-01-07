49ffb076351c53b8cf40f186b43fd1c77a99d81c
joshand@cisco.com <joshand@cisco.com>
2020-01-07 12:00:47 -0600

    fix issue with wh update; post->put

--- meraki_addons.py
+++ meraki_addons.py
@@ -33,7 +33,7 @@ def add_api_http_servers(api_key, net_id, wh_name, wh_url):
 def update_api_http_servers(api_key, net_id, wh_id, wh_name, wh_url):
     headers = {'X-Cisco-Meraki-API-Key': api_key}
     data = {"name": wh_name, "url": wh_url}
-    response = requests.post("https://api.meraki.com/api/v0/networks/" + net_id + "/httpServers/" + wh_id, data=data, headers=headers)
+    response = requests.put("https://api.meraki.com/api/v0/networks/" + net_id + "/httpServers/" + wh_id, data=data, headers=headers)
 
     if response.ok:
         return response.json()
